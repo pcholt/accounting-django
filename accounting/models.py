bca120c9910db37328e588bddef4100328f0ad26
ph <pcholt@gmail.com>
2020-11-23 19:50:08 +1100

    Model changed

--- accounting/models.py
+++ accounting/models.py
@@ -4,16 +4,18 @@ from django.contrib.auth.models import User
 from django.db.models import Q
 
 class Account(models.Model):
-	owner = models.ForeignKey(User, on_delete=models.CASCADE)
-	name = models.CharField(max_length=512)
-	balance = models.DecimalField(decimal_places=5, max_digits=20, default=0)
-	clearance_period = models.IntegerField()
+
 	TYPE = [
 	    ('DA', 'Asset'),
 	    ('DE', 'Expense'),
 	    ('CL', 'Liability'),
 	    ('CE', 'Equity'),
 	]
+
+	owner = models.ForeignKey(User, on_delete=models.CASCADE)
+	name = models.CharField(max_length=512)
+	balance = models.DecimalField(decimal_places=5, max_digits=20, default=0)
+	clearance_period = models.IntegerField()
 	account_type = models.CharField(choices=TYPE, default='DA', max_length=2)
 
 	def real_balance(self):
@@ -21,16 +23,20 @@ class Account(models.Model):
 		# is greater than this account's clearance period, or is uncleared,
 		# plus the balance on the Account.
 		total = self.balance
-		total += sum(t.amount for t in Transaction.objects.filter(debit=self))
-		total -= sum(t.amount for t in Transaction.objects.filter(credit=self))
-		if self.account_type[0]=='C':
+		total += sum(t.amount for t in Transaction.objects.filter(debit=self).filter(cleared=false))
+		total -= sum(t.amount for t in Transaction.objects.filter(credit=self).filter(cleared=false))
+		if self.is_credit():
 			total = -total
 		return total
+
+	def is_credit(self):
+		return self.account_type in ('CL', 'CE')
 
 	def __str__(self):
 		return str({"name":self.name, "owner":self.owner})
 
 class Transaction(models.Model):
+
 	debit = models.ForeignKey(Account, on_delete=models.CASCADE, related_name="debit")
 	credit = models.ForeignKey(Account, on_delete=models.CASCADE, related_name="credit")
 	cleared = models.BooleanField()
@@ -41,6 +47,7 @@ class Transaction(models.Model):
 		return "amount={}  debit {} credit {} ".format(self.amount, self.debit, self.credit) 
 
 class Clearance(models.Model):	
+
 	clearance_period = models.AutoField(primary_key = True)
 	date = models.DateTimeField(auto_now_add=True)
 
